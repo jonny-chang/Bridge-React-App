cc32a1782ed3e078bc13b8142dbd705f668dad40
Jonny Chang <jonathanchang0805@live.ca>
2020-09-26 10:46:15 -0700

    Basic setup and redux

---
+++ src/redux/store.js
@@ -0,0 +1,29 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+// Reducers
+import userReducer from './reducers/userReducer';
+import dataReducer from './reducers/dataReducer';
+
+// Init state
+const initialState = {};
+
+// Allows for async code using dispatch()
+const middleware = [thunk];
+
+const reducers = combineReducers({
+    user: userReducer,
+    data: dataReducer,
+})
+
+// Init store
+const store = createStore(
+    reducers, 
+    initialState, 
+    compose(
+        applyMiddleware(...middleware),
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+export default store
