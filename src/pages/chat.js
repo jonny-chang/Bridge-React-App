d37e2186cd941abad873c65216e10941fda8769a
Jonny Chang <jonathanchang0805@live.ca>
2020-09-27 04:32:24 -0700

    Added chat page

---
+++ src/pages/chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class chat extends Component {
+    state={}
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default chat
