4ed274cdf6ea5eeb50d2b7ef474a241300f8786c
Joshua  Lorincz <joshlordeveloper@gmail.com>
2020-09-27 07:09:37 -0700

    Minor changes

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class chat extends Component {
-    state={}
+    state={
+        message: ""
+    }
+    updateMessage = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+    _handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            fetch(`http://13.57.251.106/send-message?user=${this.props.user.email}&message=${e.target.value}&`)
+            .then(res => res.json())
+            .then(
+              (result) => {
+                  
+                }
+            )
+        }
+    }
     render(){
         const style = {
             width: "100%",
@@ -12,12 +31,34 @@ class chat extends Component {
             margin: 30,
             height: 600
         };
+        const textAreaStyle = {
+            position: "fixed",
+            width: "402px",
+            textAlign: "center",
+            left: "48%",
+            top: "80.5%",
+            fontSize: 16,
+            height: "47px"
+        };
         return(
-            <div id="talkjs-container" style={style}>
-                <i>Loading chat...</i>
+            <div> 
+                <div id="talkjs-container" style={style}>
+                    <i>Loading chat...</i>
+                </div>
+                <input style={textAreaStyle} onKeyDown={this._handleKeyDown} placeholder="Enter your message here" value={this.state.message} onChange={this.updateMessage}>
+                    
+                </input>
             </div>
         )
     }
 }
 
-export default chat
+const mapStateToProps = (state) => ({
+    user: state.user,
+})
+
+const mapActionsToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(chat);
